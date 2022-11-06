55e8a90266e940cef2048877bb13e04cb64ec759
Geheimniss <aganudaiche@gmail.com>
2022-11-06 20:46:03 +0600

    init

---
+++ WhatsEat/WhatsEat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WhatsEat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
