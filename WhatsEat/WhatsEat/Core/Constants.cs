40bbcb80868666e481bb32a8bdec5cfb26360d9e
Geheimniss <aganudaiche@gmail.com>
2022-11-26 17:46:56 +0600

    FIX: Role bug

--- WhatsEat/WhatsEat/Core/Constants.cs
+++ WhatsEat/WhatsEat/Core/Constants.cs
@@ -4,9 +4,9 @@ public static class Constants
 {
     public static class Roles
     {
-        public const string Administrator = "Administrator";
-        public const string Manager = "Manager";
-        public const string User = "User";
+        public const string Administrator = "Администратор";
+        public const string Manager = "Менеджер";
+        public const string User = "Пользователь";
     }
 
     public static class Policies
